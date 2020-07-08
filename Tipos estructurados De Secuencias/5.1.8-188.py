e3e556cd84451f8fece3046e2477f27fabb89240
Frank Casanova <frankcasanova.info@gmail.comm>
2020-07-08 22:45:30 +0200

    versión de encriptación con listas

--- Tipos estructurados De Secuencias/5.1.8-188.py
+++ Tipos estructurados De Secuencias/5.1.8-188.py
@@ -1,26 +1,3 @@
 # prgrama de encriptacion
 
-frase ='wyz' #input('Escriba la frase que desea encriptar: ')
-
-frase = frase.lower()
-
-crip1 = []
-
-for i in frase:
-
-    if i == 'z':
-        crip1.append(ord('b'))
-    elif i == 'y':
-        crip1.append(ord('a'))
-    else:     
-        crip1.append(ord(i) + 2)
-
-crip2 = []
-
-
-for i in crip1:
-    crip2.append(chr(i))
-
-
-print(''.join(crip2))
-    
+frase = input('Escriba la frase que desea encriptar: ')
