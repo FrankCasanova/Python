876f48aabf64ca7323c96338461ac9bd0df4cc7e
Frank Casanova <frankcasanova.info@gmail.comm>
2020-07-08 22:44:20 +0200

    primera verión del programa de ecriptación, usando listas

--- Tipos estructurados De Secuencias/5.1.8-188.py
+++ Tipos estructurados De Secuencias/5.1.8-188.py
@@ -1,3 +1,26 @@
 # prgrama de encriptacion
 
-frase = input('Escriba la frase que desea encriptar: ')
+frase ='wyz' #input('Escriba la frase que desea encriptar: ')
+
+frase = frase.lower()
+
+crip1 = []
+
+for i in frase:
+
+    if i == 'z':
+        crip1.append(ord('b'))
+    elif i == 'y':
+        crip1.append(ord('a'))
+    else:     
+        crip1.append(ord(i) + 2)
+
+crip2 = []
+
+
+for i in crip1:
+    crip2.append(chr(i))
+
+
+print(''.join(crip2))
+    
