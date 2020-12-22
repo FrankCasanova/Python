55cdb9a45342dfe9f7927a39e11538d298373c0e
Frank Casanova <frankcasanova.info@gmail.com>
2020-12-23 00:17:54 +0100

    para huelva

--- FICHEROS/445.py
+++ FICHEROS/445.py
@@ -11,8 +11,9 @@ def word_in_file(aList, files):
     """
     return a list with the lines 
     that contain a list of given words
-
-    """
+   """
+    
+#%%    
     with open(files, 'r') as file:
 
         for index, line in enumerate(file):
@@ -20,7 +21,7 @@ def word_in_file(aList, files):
                 if word in line:
                     print(f'La palabra {word} aparece en la línea {index}')
 
-
+#%%
 file_name = 'Bio.txt'
 words = ['Bueno', 'batería']
 
